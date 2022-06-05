87f312ccab3a38eb0c63d546c1bbf5be1a61ac5a
THANH THONG <THANH THONG@DESKTOP-3B2ASLS>
2022-06-05 19:23:28 +0700

    Add project files.

---
+++ LapBigSchool-VoThanhThong/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LapBigSchool_VoThanhThong.Startup))]
+namespace LapBigSchool_VoThanhThong
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ LapBigSchool-VoThanhThong/ViewModels/CourseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using LapBigSchool_VoThanhThong.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace LapBigSchool_VoThanhThong.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        string day = "dd/M/yyyy";
+        string tim = "HH:mm";
+        public DateTime GetDateTime()
+        {
+            return DateTime.ParseExact(string.Format("{0} {1}", Date, Time), string.Format("{0} {1}", day, tim), CultureInfo.CurrentCulture);
+        }
+    }
+}
