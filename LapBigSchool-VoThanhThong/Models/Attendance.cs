3c1b5e91eac6d3aa655ceed4cbc113fdb7c3f8a2
THONG VO <vothanhthong2001@gmail.com>
2022-06-10 15:08:44 +0700

    fdtr

---
+++ LapBigSchool-VoThanhThong/Models/Attendance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LapBigSchool_VoThanhThong.Models
+{
+    public class Attendance
+    {
+    }
+    public Course Course { get; set; }
+    [Key]
+    [column(Order = 1)]
+
+}
